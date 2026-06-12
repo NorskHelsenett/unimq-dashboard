4f8b5a862dc96b1013d0f01a4ee3e00e06d10d87
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-06-12 17:58:58 +0200

    added config for mongo and prometheus

---
+++ volumes-for-compose/mongodb/mongo-init.js
@@ -0,0 +1,26 @@
+let res = [
+    db.createCollection("alarms"),
+    db.createCollection("maintenance"),
+    db.createCollection("notifications"),
+
+    db.createRole({
+        role: "roleApiUser",
+        privileges: [
+            {
+                resource: { db: "unimq", collection: "alarms" },
+                actions: ["find", "insert", "update", "remove"],
+            },
+            {
+                resource: { db: "unimq", collection: "maintenance" },
+                actions: ["find", "insert", "update", "remove"],
+            },
+            {
+                resource: { db: "unimq", collection: "notifications" },
+                actions: ["find", "insert", "update", "remove"],
+            },
+        ],
+        roles: [{ role: "readWrite", db: "unimq" }],
+    }),
+];
+
+printjson(res);
