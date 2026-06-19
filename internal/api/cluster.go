f54d35c97239f15613febfbc3587edee7f8c11a0
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-06-19 09:36:32 +0200

    added swagger comments

--- internal/api/cluster.go
+++ internal/api/cluster.go
@@ -6,6 +6,13 @@ import (
 	"github.com/sisneve/rabbitmq-dashboard/internal/routes/httpsuite"
 )
 
+// @Summary		Get Cluster Stats
+// @Description	Get overall cluster statistics and health information
+// @Tags			Cluster
+// @Produce		json
+// @Success		200	{object}	models.ClusterStats
+// @Failure		500	{object}	httpsuite.APIError
+// @Router			/v1/cluster [get]
 func (rc *APIService) GetClusterHandler(w http.ResponseWriter, r *http.Request) {
 	stats, err := rc.RMQClient.GetClusterStats()
 	if err != nil {
