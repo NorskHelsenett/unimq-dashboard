d4b838c80d83e917c34ddd57432d714542bc4740
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-06-26 12:03:58 +0200

    swagger docs, created PostMaintenanceEntry to simplify user input

--- internal/models/maintenance.go
+++ internal/models/maintenance.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"slices"
 	"time"
+
+	"github.com/google/uuid"
 )
 
 type MaintenanceStatus string
@@ -52,23 +54,53 @@ func IsValidMaintenanceStatus(s string) bool {
 	return slices.Contains(statuses, MaintenanceStatus(s))
 }
 
+type PostMaintenanceEntry struct {
+	Description string `json:"description" bson:"description" example:"maintenance for server upgrade"`
+	Start       string `json:"start" bson:"start" example:"2024-06-01 10:00:00"`
+	End         string `json:"end" bson:"end" example:"2024-06-01 12:00:00"`
+}
+
+func (p *PostMaintenanceEntry) ToMaintenanceEntry() (*MaintenanceEntry, error) {
+
+	start, err := time.Parse(timeStampLayout, p.Start)
+	if err != nil {
+		return nil, fmt.Errorf("invalid start time format: %w", err)
+	}
+
+	end, err := time.Parse(timeStampLayout, p.End)
+	if err != nil {
+		return nil, fmt.Errorf("invalid end time format: %w", err)
+	}
+
+	return &MaintenanceEntry{
+		ID:          uuid.New().String(),
+		Description: p.Description,
+		Start:       start,
+		End:         end,
+		Status:      MaintenanceStatusScheduled,
+		Notified:    false,
+	}, nil
+}
+
 type MaintenanceEntry struct {
-	ID          string            `json:"id" bson:"_id"`
-	Description string            `json:"description" bson:"description"`
-	Start       time.Time         `json:"start" bson:"start"`
-	End         time.Time         `json:"end" bson:"end"`
-	Status      MaintenanceStatus `json:"status" bson:"status"`
+	ID          string            `json:"id" bson:"_id" example:""`
+	Description string            `json:"description" bson:"description" example:"maintenance for server upgrade"`
+	Start       time.Time         `json:"start" bson:"start" example:"2024-06-01 10:00:00"`
+	End         time.Time         `json:"end" bson:"end" example:"2024-06-01 12:00:00"`
+	Status      MaintenanceStatus `json:"status" bson:"status" example:"-"`
 	Notified    bool              `json:"notified" bson:"notified"`
 }
 
+const timeStampLayout = "2006-01-02 15:04:05"
+
 func (e *MaintenanceEntry) UnmarshalJSON(data []byte) error {
 	var aux struct {
-		ID          string            `json:"id"`
-		Description string            `json:"description"`
-		Start       string            `json:"start"`
-		End         string            `json:"end"`
-		Status      MaintenanceStatus `json:"status"`
-		Notified    bool              `json:"notified"`
+		ID          string `json:"id"`
+		Description string `json:"description"`
+		Start       string `json:"start"`
+		End         string `json:"end"`
+		Status      string `json:"status"`
+		Notified    bool   `json:"notified"`
 	}
 
 	if err := json.Unmarshal(data, &aux); err != nil {
@@ -76,15 +108,22 @@ func (e *MaintenanceEntry) UnmarshalJSON(data []byte) error {
 	}
 
 	var err error
-	e.Start, err = time.Parse(time.RFC3339, aux.Start)
+	e.Start, err = time.Parse(timeStampLayout, aux.Start)
 	if err != nil {
 		return fmt.Errorf("invalid start time format: %w", err)
 	}
 
-	e.End, err = time.Parse(time.RFC3339, aux.End)
+	e.End, err = time.Parse(timeStampLayout, aux.End)
 	if err != nil {
 		return fmt.Errorf("invalid end time format: %w", err)
 	}
+
+	e.Description = aux.Description
+	ok := IsValidMaintenanceStatus(aux.Status)
+	if !ok {
+		return fmt.Errorf("invalid maintenance status: %s, expected any of %v", aux.Status, GetMaintenanceStatusAllString())
+	}
+	e.Notified = false
 
 	return nil
 }
@@ -121,8 +160,10 @@ func NewMaintenanceResponse(scheduled []MaintenanceEntry, history []MaintenanceE
 	}
 }
 
+// Update status model for maintenance entry
+// @Description	Update the status of a maintenance entry (scheduled, done, skipped)
 type UpdateMaintenance struct {
-	Status MaintenanceStatus `json:"status"`
+	Status MaintenanceStatus `json:"status"` // Enums - scheduled, done, skipped
 }
 
 func (u *UpdateMaintenance) UnmarshalJSON(data []byte) error {
