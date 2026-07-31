b4be7f4122b5ca8a10e11e3f1779b63ee6e30fab
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-07-31 15:03:08 +0200

    added ID and AlarmType to LogEntry, added constructor to LogEntry

--- internal/models/alarms.go
+++ internal/models/alarms.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type AlarmEntry struct {
 	AlarmID string     `bson:"_id"`
@@ -15,8 +19,21 @@ const (
 )
 
 type LogEntry struct {
+	ID        string    `json:"id" bson:"id"`
 	Timestamp time.Time `json:"ts"`
 	Event     LogEvent  `json:"event"`
 	Value     *float64  `json:"value,omitempty"`
 	Threshold float64   `json:"threshold"`
+	AlarmType AlarmType `json:"alarm_type"`
+}
+
+func NewLogEntry(event LogEvent, value *float64, threshold float64, alarmType AlarmType) LogEntry {
+	return LogEntry{
+		ID:        uuid.New().String(),
+		Timestamp: time.Now(),
+		Event:     event,
+		Value:     value,
+		Threshold: threshold,
+		AlarmType: alarmType,
+	}
 }
