77fd1b38abdf2adf235bb0e5d727a1440fa5cc3a
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-08-28 14:00:33 +0200

    restructured the routes tto allow for authentation on protected routes, dex client

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -10,41 +10,33 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	api "github.com/sisneve/rabbitmq-dashboard/internal/api/v1"
+	"github.com/sisneve/rabbitmq-dashboard/internal/clients/dex"
 	"github.com/sisneve/rabbitmq-dashboard/internal/clients/prometheus"
 	"github.com/sisneve/rabbitmq-dashboard/internal/clients/rabbitmq"
 	"github.com/sisneve/rabbitmq-dashboard/internal/config"
+	"github.com/sisneve/rabbitmq-dashboard/internal/controllers/authcontroller"
 	"github.com/sisneve/rabbitmq-dashboard/internal/database"
 	_ "github.com/sisneve/rabbitmq-dashboard/internal/docs"
-	"github.com/sisneve/rabbitmq-dashboard/internal/models"
-	"github.com/sisneve/rabbitmq-dashboard/internal/notify"
-	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
 
-func SetupRoutes(ctx context.Context, config *config.Config, db *database.Database, rmq *rabbitmq.RMQClient, checker *notify.Checker) (chi.Router, error) {
-	r := chi.NewRouter()
-
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.Timeout(60 * time.Second))
-	r.Use(middleware.RequestID)
+func SetupRoutes(ctx context.Context, config *config.Config, db *database.Database, rmq *rabbitmq.RMQClient) (chi.Router, error) {
 
 	prom, err := prometheus.NewPromClient(config.PrometheusHost, "v1", "", "", config.PrometheusPort)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create Prometheus client: %w", err)
 	}
 
-	limits := &models.Limits{
-		MaxChannels:    config.RabbitMQChannelLimit,
-		MaxConnections: config.RabbitMQConnectionLimit,
-		MaxQueues:      config.RabbitMQQueueLimit,
+	dex, err := dex.NewDexClient(ctx, config.OIDC)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create Dex client: %w", err)
 	}
 
 	apiservice, err := api.NewAPIService(
 		api.WithContext(ctx),
 		api.WithRabbitMQClient(rmq),
 		api.WithPromClient(prom),
+		api.WithDexClient(dex),
 		api.WithDatabase(db),
-		api.WithRMQLimits(limits),
 		api.WithEmailConfig(config.Email),
 		api.WithChecker(checker),
 	)
@@ -52,23 +44,36 @@ func SetupRoutes(ctx context.Context, config *config.Config, db *database.Databa
 		return nil, fmt.Errorf("failed to create API service: %w", err)
 	}
 
-	r.Route("/api", func(r chi.Router) {
-		r.Get("/swagger/*", httpSwagger.WrapHandler)
-		SetupV1Routes(r, apiservice)
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(60 * time.Second))
+
+	r.Group(func(r chi.Router) {
+		r.Route("/api", func(r chi.Router) {
+			SetupUnprotectedRoutes(r, apiservice)
+
+			r.Group(func(r chi.Router) {
+				r.Use(authcontroller.AuthenticationMiddleware)
+				SetupProtectedRoutes(r, apiservice)
+			})
+		})
 	})
 
 	routeCount := 0
 	// Logs every route implicitly or explicitly defined above with its method, path, and number of middlewares.
 	err = chi.Walk(r, func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
 		routeCount++
-		slog.Info("route info", "method", method, "route", route, "middlewares", len(r.Middlewares()))
+		slog.InfoContext(ctx, "route info", "method", method, "route", route, "middlewares", len(middlewares))
 		return nil
 	})
 	if err != nil {
 		return nil, fmt.Errorf("failed to walk routes: %w", err)
 	}
 
-	slog.Info("total routes registered", "count", routeCount)
+	slog.InfoContext(ctx, "total routes registered", "count", routeCount)
 
 	return r, nil
 
