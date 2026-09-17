4573ada8779251dbee426656d210d3c76753a306
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-09-17 15:07:41 +0200

    removed guard for check on already set default logger, now just sets it anyways if it's the same

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -36,10 +36,6 @@ func SetupLogger() {
 
 func UpdateLogLevel(level slog.Level) {
 
-	if slog.Default().Enabled(context.TODO(), level) {
-		return
-	}
-
 	slog.Error("updating log level", "level", level.String())
 
 	handlerOpts := getHandlerOpts(level)
