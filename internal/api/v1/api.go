fde50dcf94d456ae5f54527fa7ca87d599dc264b
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-09-18 14:01:41 +0200

    comments, started profile page for the API

--- internal/api/v1/api.go
+++ internal/api/v1/api.go
@@ -12,6 +12,7 @@ import (
 	"github.com/sisneve/rabbitmq-dashboard/internal/notify"
 )
 
+// TODO: This has become a singleton, and should be refactored to be a proper service with a constructor and options.
 type APIService struct {
 	Ctx         context.Context
 	RMQClient   *rabbitmq.RMQClient
---
+++ internal/api/v1/profile.go
@@ -0,0 +1,4 @@
+package api
+
+// TODO: This is going to become the profile service.
+// Currently empty as it's not been implemented.
