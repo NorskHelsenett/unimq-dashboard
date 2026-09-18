9bc9ab6be8135f66a31ff668436b4c4cfd654438
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-09-18 08:52:36 +0200

    split maintenancelogs into its own file

---
+++ internal/database/maintenancelogs.go
@@ -0,0 +1,86 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"log/slog"
+	"time"
+
+	"github.com/sisneve/rabbitmq-dashboard/internal/models"
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+func (dbc *Database) GetMaintenanceEditLogsAll(ctx context.Context) ([]models.MaintenanceEditLog, error) {
+	start := time.Now()
+
+	cursor, err := dbc.Collections.MaintenanceEditLogs.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		err := cursor.Close(ctx)
+		if err != nil {
+			slog.ErrorContext(ctx, "failed to close cursor", "runtime", time.Since(start), "error", err)
+		}
+	}()
+
+	var logs []models.MaintenanceEditLog
+	err = cursor.All(ctx, &logs)
+	if err != nil {
+		return nil, err
+	}
+
+	slog.DebugContext(ctx, "retrieved maintenance edit logs", "runtime", time.Since(start), "count", len(logs))
+	return logs, nil
+}
+
+func (dbc *Database) GetMaintenanceEditLogs(ctx context.Context, maintenanceID string) ([]models.MaintenanceEditLog, error) {
+	start := time.Now()
+	opts := bson.D{{Key: "maintenance_id", Value: maintenanceID}}
+	cursor, err := dbc.Collections.MaintenanceEditLogs.Find(ctx, opts)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := cursor.Close(ctx); err != nil {
+			slog.ErrorContext(ctx, "failed to close cursor", "runtime", time.Since(start), "error", err)
+		}
+	}()
+
+	var logs []models.MaintenanceEditLog
+	if err = cursor.All(ctx, &logs); err != nil {
+		slog.ErrorContext(ctx, "failed to decode maintenance edit logs", "runtime", time.Since(start), "maintenance_id", maintenanceID, "error", err)
+		return nil, err
+	}
+	slog.DebugContext(ctx, "retrieved maintenance edit logs", "runtime", time.Since(start), "maintenance_id", maintenanceID, "count", len(logs))
+	return logs, nil
+}
+
+func (dbc *Database) AddMaintenanceEditLog(ctx context.Context, entry *models.MaintenanceEditLog) error {
+	start := time.Now()
+	_, err := dbc.Collections.MaintenanceEditLogs.InsertOne(ctx, entry)
+	if err != nil {
+		slog.ErrorContext(ctx, "failed to insert maintenance edit log", "runtime", time.Since(start), "maintenance_id", entry.MaintenanceID, "error", err)
+	} else {
+		slog.DebugContext(ctx, "inserted maintenance edit log", "runtime", time.Since(start), "maintenance_id", entry.MaintenanceID)
+	}
+	return err
+}
+
+func (dbc *Database) DeleteMaintenanceEntry(ctx context.Context, entryID string) error {
+	start := time.Now()
+
+	status, err := dbc.Collections.Maintenance.DeleteOne(ctx, map[string]any{id: entryID})
+	if err != nil {
+		slog.ErrorContext(ctx, "failed to delete maintenance", "runtime", time.Since(start), id, entryID, "error", err)
+		return err
+	}
+	if status.DeletedCount == 0 {
+		slog.ErrorContext(ctx, "no maintenance entry found to delete", "runtime", time.Since(start), id, entryID)
+		return fmt.Errorf("%w, with id: %s", ErrMaintenanceNotFound, entryID)
+	}
+
+	slog.DebugContext(ctx, "deleted maintenance", "runtime", time.Since(start), id, entryID)
+	return nil
+}
