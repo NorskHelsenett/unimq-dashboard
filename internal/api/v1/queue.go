9aa4fd24a6fedcd2222651167a51e0e4f02fd3ac
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-09-22 16:10:14 +0200

    swagger comments

--- internal/api/v1/queue.go
+++ internal/api/v1/queue.go
@@ -15,7 +15,7 @@ import (
 // @Tags			Queues
 // @Produce		json
 // @Param			vhost-name	path		string						true	"Virtual Host"
-// @Success		200			{object}	[]models.QueueAPIResponse	"HTML page with queue metrics"
+// @Success		200			{object}	[]models.RMQQueue	"HTML page with queue metrics"
 // @Failure		400			{object}	httpsuite.ErrorResponse		"Bad Request"
 // @Failure		401			{object}	httpsuite.ErrorResponse
 // @Failure		403			{object}	httpsuite.ErrorResponse
