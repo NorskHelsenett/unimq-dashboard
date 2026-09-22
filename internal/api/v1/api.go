c977ee945c53bc82730ad93922e616210e9c6083
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-09-22 16:09:20 +0200

    removed static limits, new limits are gathered from RMQ, removed prom client

--- internal/api/v1/api.go
+++ internal/api/v1/api.go
@@ -4,21 +4,16 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/sisneve/rabbitmq-dashboard/internal/clients/prometheus"
 	"github.com/sisneve/rabbitmq-dashboard/internal/clients/rabbitmq"
 	"github.com/sisneve/rabbitmq-dashboard/internal/database"
-	"github.com/sisneve/rabbitmq-dashboard/internal/models"
 	"github.com/sisneve/rabbitmq-dashboard/internal/notify"
 )
 
 // TODO: This has become a singleton, and should be refactored to be a proper service with a constructor and options.
 type APIService struct {
-	Ctx        context.Context
-	RMQClient  *rabbitmq.RMQClient
-	PromClient *prometheus.PromClient
-	// DexClient   *dex.DexClient
+	Ctx         context.Context
+	RMQClient   *rabbitmq.RMQClient
 	DB          *database.Database
-	RMQLimits   *models.Limits
 	AdminGroups []string
 	Checker     *notify.Checker
 }
@@ -38,20 +33,6 @@ func WithRabbitMQClient(rmq *rabbitmq.RMQClient) APIServiceOption {
 		return nil
 	}
 }
-
-func WithPromClient(prom *prometheus.PromClient) APIServiceOption {
-	return func(rc *APIService) error {
-		rc.PromClient = prom
-		return nil
-	}
-}
-
-// func WithDexClient(dex *dex.DexClient) APIServiceOption {
-// 	return func(rc *APIService) error {
-// 		rc.DexClient = dex
-// 		return nil
-// 	}
-// }
 
 func WithDatabase(db *database.Database) APIServiceOption {
 	return func(rc *APIService) error {
@@ -76,12 +57,9 @@ func WithAdminGroups(groups []string) APIServiceOption {
 
 func newAPIServiceConfig() *APIService {
 	return &APIService{
-		Ctx:        context.Background(),
-		RMQClient:  nil,
-		PromClient: nil,
-		// DexClient:   nil,
+		Ctx:         context.Background(),
+		RMQClient:   nil,
 		DB:          nil,
-		RMQLimits:   nil,
 		AdminGroups: []string{},
 		Checker:     nil,
 	}
