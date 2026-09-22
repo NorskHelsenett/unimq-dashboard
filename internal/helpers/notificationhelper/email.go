bdd2acf1c5eb190cf2520c8ad0009a55fe2780ad
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-09-22 09:02:07 +0200

    split webhook and email into their own files

---
+++ internal/helpers/notificationhelper/email.go
@@ -0,0 +1,89 @@
+package notificationhelper
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/sisneve/rabbitmq-dashboard/internal/config"
+	"github.com/wneessen/go-mail"
+)
+
+var (
+	ErrEmailNotConfigured = fmt.Errorf("SMTP server is not configured")
+	ErrEmailSendFailed    = fmt.Errorf("failed to send email")
+)
+
+type EmailStatus struct {
+	Recipient string `json:"recipient"`
+	OK        bool   `json:"ok"`
+	Error     error  `json:"error"`
+}
+
+func sendEmail(config *config.EmailConfig, to, subject, body string, typ mail.ContentType) *EmailStatus {
+	status := &EmailStatus{
+		Recipient: to,
+		OK:        false,
+		Error:     nil,
+	}
+
+	if config == nil {
+		status.Error = ErrEmailNotConfigured
+		return status
+	}
+
+	if config.EmailFromAddress == "" {
+		status.Error = ErrEmailNotConfigured
+		return status
+	}
+	message := mail.NewMsg()
+	if err := message.From(config.EmailFromAddress); err != nil {
+		status.Error = fmt.Errorf("failed to set From address: %w", err)
+		return status
+	}
+	if err := message.To(to); err != nil {
+		status.Error = fmt.Errorf("failed to set To address: %w", err)
+		return status
+	}
+
+	message.Subject(subject)
+	message.SetBodyString(typ, body)
+
+	client, err := mail.NewClient(config.EmailSMTPHost)
+	if err != nil {
+		status.Error = fmt.Errorf("failed to create mail client: %w", err)
+	}
+
+	if err := client.DialAndSend(message); err != nil {
+		status.Error = fmt.Errorf("failed to send mail: %w", err)
+		return status
+	}
+
+	status.OK = true
+	return status
+}
+
+type EmailSender struct {
+	Config *config.EmailConfig
+}
+
+var EmailSenderInstance *EmailSender
+
+func InitEmailSender(config *config.EmailConfig) {
+	EmailSenderInstance = &EmailSender{
+		Config: config,
+	}
+}
+
+func (es *EmailSender) SendEmail(to, subject, body string, typ mail.ContentType) *EmailStatus {
+	return sendEmail(es.Config, to, subject, body, typ)
+}
+
+func (es *EmailSender) SendEmails(ctx context.Context, to []string, subject, body string, typ mail.ContentType) []EmailStatus {
+	var statuses []EmailStatus
+	for _, email := range to {
+		status := es.SendEmail(email, subject, body, typ)
+		statuses = append(statuses, *status)
+	}
+
+	return statuses
+}
---
+++ internal/helpers/notificationhelper/webhook.go
@@ -0,0 +1,66 @@
+package notificationhelper
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+type WebhookStatus struct {
+	URL   string `json:"url"`
+	OK    bool   `json:"ok"`
+	Error error  `json:"error"`
+}
+
+func SendWebhook(ctx context.Context, url string, subject, body string) *WebhookStatus {
+	status := &WebhookStatus{
+		URL:   url,
+		OK:    false,
+		Error: nil,
+	}
+	text := subject + "\n\n" + body
+	payload, _ := json.Marshal(map[string]string{"text": text})
+	tctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	req, err := http.NewRequestWithContext(tctx, http.MethodPost, url, bytes.NewBuffer(payload))
+	if err != nil {
+		status.Error = fmt.Errorf("failed to create request for webhook %s: %w", url, err)
+		slog.ErrorContext(ctx, "failed to create request for webhook", "url", url, "error", err)
+		return status
+	}
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		status.Error = fmt.Errorf("failed to send request to webhook %s: %w", url, err)
+		slog.ErrorContext(ctx, "failed to send request to webhook", "url", url, "error", err)
+		return status
+	}
+	defer func() {
+		err := resp.Body.Close()
+		if err != nil {
+			slog.ErrorContext(ctx, "failed to close response body for webhook", "url", url, "error", err)
+		}
+	}()
+
+	if resp.StatusCode >= 400 {
+		status.Error = fmt.Errorf("webhook returned %d", resp.StatusCode)
+		slog.ErrorContext(ctx, "webhook returned error status code", "url", url, "status_code", resp.StatusCode)
+		return status
+	}
+	status.OK = true
+	return status
+}
+
+func SendWebhooks(urls []string, subject, body string) []WebhookStatus {
+	var statuses []WebhookStatus
+	for _, url := range urls {
+		status := SendWebhook(context.Background(), url, subject, body)
+		statuses = append(statuses, *status)
+	}
+
+	return statuses
+}
