0cb4e962231a764faeadc07bf8cbc0b0d93b1474
Kjetil Sigvartsen <kjetil.sigvartsen@nhn.no>
2026-09-22 16:10:28 +0200

    added authentication handlers

---
+++ internal/api/routes/authentication.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/sisneve/rabbitmq-dashboard/internal/clients/dex"
+)
+
+func SetupAuthenticationRoutes(r chi.Router, dex *dex.DexClient) {
+
+	r.Route("/login", func(r chi.Router) {
+		r.Get("/", dex.RedirectHandler)
+		r.Get("/callback", dex.OauthCallbackHandler)
+	})
+}
